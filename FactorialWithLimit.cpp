4c8981fbb951a8ac2b35e6eb2953ed1aec917139
MUHAMMAD SHAHZAD ARSHAD <155644365+ShahzadArshad786@users.noreply.github.com>
2024-06-12 08:49:55 +0500

    Add files via upload

---
+++ FactorialWithLimit.cpp
@@ -0,0 +1,29 @@
+/* Write a Program using C++ that inputs end/limit of loop
+   and print all numbers factorial from 1 to input limit. 
+*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+
+    int limit , i , j  , fact ; 
+
+    cout<<"Enter an Positive Number : " ;
+    cin>>limit ; 
+
+cout<<"\n"<<setw(5)<<"Number "<<setw(20)<<"Factorial\n\n" ; 
+    for ( i = 1 ; i <= limit ; i++)
+    {
+        fact = 1 ;
+
+        for( j = 1 ; j <= i ; j++)
+        {
+        fact = fact * j ; 
+        }
+    cout<<setw(3)<<i<<" !"<< setw(8)<<" :="<<setw(8)<<fact<<endl;
+
+    }
+    return 0 ; 
+}
