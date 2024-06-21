137250b7c574a85d9180e36d2269a43d594d20e7
Your Name <you@example.com>
2024-06-21 15:52:50 +0500

    "first commit"

---
+++ PrintDigitsofNumber.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+    int num , j , digit ,  k ; 
+    
+    j = 0 ; 
+    k = 1 ; 
+ 
+    cout<<"Enter Any Number : ";
+    cin>> num ; 
+ 
+    int n = num ; 
+    // convert number into string
+    string str = to_string(n) ; 
+
+    // find string length 
+    int length = str.length(); 
+
+    // declare arr with equal size of length of number 
+    int arr[length] ;
+
+    for (int i = num ; i != 0 ; i = i / 10)
+    {
+        digit = i % 10 ;
+        arr[j] = digit ; 
+        j++ ; 
+    }
+ 
+    cout <<"\nDigits of a Number "<<n<< " are : \n\n"; 
+
+    for(int i = j - 1 ; i >= 0 ; i--)
+    {
+        cout<<"Digit "<<k<< " : "<<arr[i]<<endl; 
+        k++ ; 
+    }
+ 
+    return 0;
+}
